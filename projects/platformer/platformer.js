6c4cc6f491c35ff7f3eaa1a62a8ee7613789816d
Eriana straughter <eriana.straughter@nextlevelnola.org>
2023-08-24 20:49:46 +0000

    platformer

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,7 +43,11 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
+    createPlatform(800, 400, 200, 20);
+    createPlatform(400,500,200, 20);
+    createPlatform(700,600,200,20);
+    createPlatform(400,300,200, 20);
+    createPlatform(800,200,300,20);
 
 
     
@@ -51,18 +55,19 @@ $(function () {
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
-    // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
+    // example us age: createCollectable(type, x, y, gravity, bounce)
+    createCollectable("max",400,600,); 
+    createCollectable("kennedi",900,100,);
+    createCollectable("grace",800, 300,);
 
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
+    createCannon("right", 600, 4000);
+    createCannon("top" , 700,4000);
+    createCannon("left" ,300, 4000);
 
 
     /////////////////////////////////////////////////
